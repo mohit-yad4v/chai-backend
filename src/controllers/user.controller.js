3effa98004f7ffc4cfc85e4373a2580916793dc7
Mohit Yadav <101042725+mohit-yad4v@users.noreply.github.com>
2025-07-07 01:29:17 +0530

    Update user.controller.js

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -181,7 +181,7 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
     try {
         const decodedToken = jwt.verify(
             incomingRefreshToken,
-            process.env.REFRESH.TOKEN.SECRET
+            process.env.REFRESH_TOKEN_SECRET
         )
 
         const user = await User.findById(decodedToken?._id)
